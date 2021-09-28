428e729ae51e5f0d6b8dcc057bd16785f5360f0b
Tom Hayton <t.hayton37@gmail.com>
2021-09-28 13:56:02 +0100

    wrapped about section in section tag, changed copy, added quotation marks, shortened quote, added transition duration, changed inline style tags to css styling

--- client/src/components/AboutSection.tsx
+++ client/src/components/AboutSection.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import styles from '../styles/css/about.module.css';
-
 import Me_500 from '../assets/photos/me_grayscale_500.png';
 import Me_768 from '../assets/photos/me_grayscale_768.png';
 import Me_1024 from '../assets/photos/me_grayscale_1024.png';
@@ -11,19 +9,29 @@ interface Props {};
 
 function AboutSection(props: Props) {
     const {} = props;
-    const aspectRatio = 500 / 548;
 
     return (
-        <>
-            <h3 style={{textAlign: "center"}}>I’m a full-stack web developer, specialising in (but not limited to) working with MERN stack technologies.<br/>Please have a snoop around any of my projects and if you like what you see, get in touch! </h3>
-            <div className={`${styles.aboutContainer}`}>
+        <section id="about" className={styles.aboutContainer}>
+            <h3>
+                I’m a full-stack web developer, specialising in (but not limited to) working with MERN stack technologies.<br/>Please have a snoop around any of my projects and if you like what you see, get in touch!
+            </h3>
+            <div className={`${styles.contentContainer}`}>
+                <div className={styles.aboutContent}>
                 <p> 
-                    I’ve been designing and building personal projects on the web for just over a year now, after leaving my career in engineering to follow the work that I am most passionate about. My academic and professional experience as an engineer has given me plenty of skills that I find myself using in my work as a web developer. Having plenty of experience working with teams of various sizes, clients of many different backgrounds and managing a wide spectrum of projects, I have a rounded skillset that gives me the tools and confidence to take on any challenge. 
+                    I’ve been filling up my portfolio with personal projects for just over a year, after leaving my career in engineering, to follow the work that I am most passionate about.
                 </p>
-                <div className={`${styles.imgContainer}`} style={{width: 500}}>
+                <p> 
+                    I've undertaken a wide range of projects that have all tested and cemented my skills in many different ways, from creative design, to technical implementation.
+                </p>
+                <p>
+                    My academic and professional experience as an engineer has given me plenty of skills that I find myself using in my work as a web developer. Having plenty of experience working with teams of various sizes, clients of many different backgrounds and managing a wide spectrum of projects, I have a rounded skillset that gives me the tools and confidence to take on any challenge.
+                </p>
+
+                </div>
+                <div className={`${styles.imgContainer}`} >
                     <img 
                         src={Me_500} 
-                        alt="foo"
+                        alt="Tom Hayton, full-stack website developer"
                         className={`${""}`}
                         srcSet={`
                             ${Me_500} 500w,
@@ -36,9 +44,12 @@ function AboutSection(props: Props) {
                     <SVGMask />
                 </div>
             </div>
-            <h3 style={{textAlign: "center"}}> We are what we repeatedly do...<br/>Excellence, therefore, isn't just an act, but a habit 
-            <br/>And life isn't just a series of events, but an ongoing process of self-definition.</h3>
-        </>
+            <h3 className={styles.quote}>
+                <span>We are what we repeatedly do...<br/>Excellence, therefore, isn't just an act, but a habit</span>
+                <br/><br/>
+                - Aristotle
+            </h3>
+        </section>
     );
 };
 
