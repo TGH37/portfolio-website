ba29a9f0717b86d6f8ab7110d0350f1628033c9e
Tom Hayton <t.hayton37@gmail.com>
2022-02-09 20:25:17 +0000

    Added skill category cards for skills section

---
+++ client/src/components/SkillCategoryCard.tsx
@@ -0,0 +1,73 @@
+import React, { CSSProperties, useMemo } from 'react';
+import styles from '../styles/css/skills.module.css';
+
+interface Props {
+    skillSectionClickHandler: Function
+    icon: {src: string, alt: string}
+    title: string
+    skillsList: string[]
+    isActive: boolean
+    cardIdx: number
+};
+
+function SkillCategoryCard(props: Props) {
+    const {
+        skillSectionClickHandler,
+        icon,
+        title,
+        skillsList,
+        isActive,
+        cardIdx
+    } = props;
+
+    type CardConfig = "top-left" | "top-right" | "bottom-left" | "bottom-right";
+
+    const borderMap: Map<CardConfig, string[]> = new Map([
+        ["top-left", ["Bottom", "Right"]],
+        ["top-right", ["Bottom", "Left"]],
+        ["bottom-left", ["Top", "Right"]],
+        ["bottom-right", ["Top", "Left"]],
+    ]);
+
+    let cardPosition: CardConfig | null = null;
+
+    switch(cardIdx) {
+        case(0):
+            cardPosition = "top-left";
+            break;
+        case(1):
+            cardPosition = "top-right";
+            break;
+        case(2):
+            cardPosition = "bottom-left";
+            break;
+        case(3):
+            cardPosition = "bottom-right";
+            break;
+        default:
+            return null
+    };
+
+    const getCardStyle = (): CSSProperties => {
+        if(cardPosition === null || isActive) return {};
+        const borderSettings = `1px solid #cecece`;
+        const borderArry = borderMap.get(cardPosition);
+        const borderKeys = borderArry.map(border => `border${border}`);
+        const styleObj = borderKeys.reduce((tot, curr)=> (tot[curr]=borderSettings, tot), {borderRadius: 0});
+        console.log(styleObj)
+        return styleObj;
+    };
+
+    const skills = skillsList.map(skill => <li key={skill}>{skill}</li>);
+
+    return (
+        <div className={`${styles.skillCategoryCard} ${isActive ? styles.activeSkillsCard : ""}`} onClick={() => skillSectionClickHandler(title)} style={getCardStyle()}>
+            <img src={icon.src} alt={icon.alt} className={styles.skillCategoryBgImg} loading="lazy"/>
+            <ul className={styles.skillsList}>
+                {skills}
+            </ul>
+        </div>
+    );
+};
+
+export default SkillCategoryCard;
--- client/src/components/SkillsSection.tsx
+++ client/src/components/SkillsSection.tsx
@@ -1,41 +1,37 @@
-/**
- * 
- */
+import React, { useEffect, useState } from 'react';
+import styles from '../styles/css/skills.module.css';
+import frontEndSVG from '../assets/svg/front-end.svg';
+import backEndSVG from '../assets/svg/back-end.svg';
+import databaseSVG from '../assets/svg/database.svg';
+import easelSVG from '../assets/svg/easel.svg';
+import SkillCategoryCard from './SkillCategoryCard';
 
- import React, { useEffect, useState } from 'react';
- import styles from '../styles/css/skills.module.css';
- import frontEndSVG from '../assets/svg/front-end.svg'
- import backEndSVG from '../assets/svg/back-end.svg'
- import databaseSVG from '../assets/svg/database.svg'
- import easelSVG from '../assets/svg/easel.svg'
 
- type HardSkill = "frontend" | "backend" | "auxiliary" | "design";
 
  function SkillsSection() {
 
     const [activeSkillset, setActiveSkillset] = useState<HardSkill | null>(null);
     const [activeSkillTitle, setActiveSkillTitle] = useState<string | null>(null);
-    const skillSectionClickHandler = (skill: HardSkill) => {
-        setActiveSkillset(skill);
-    }
+    const skillSectionClickHandler = (skill: HardSkill) => setActiveSkillset(skill);
+
     const updateSkillTitle = () => {
         setActiveSkillTitle(() => {
             switch(activeSkillset) {
                 case "frontend":
-                    return "Frontend"
+                    return "Frontend";
                 case "backend":
-                    return "Backend and Database"
+                    return "Backend";
                 case "auxiliary":
-                    return "Build Tools"
+                    return "Tools";
                 case "design":
-                    return "Design Tools"
+                    return "Design";
                 default:
-                    return ""
-            }
-        })
-    }
+                    return "Frontend";
+            };
+        });
+    };
 
-    useEffect(() => updateSkillTitle(), [activeSkillset])
+    useEffect(() => updateSkillTitle(), [activeSkillset]);
 
     return(
         <section id="skills">
@@ -43,54 +39,54 @@
             <div className={styles.skillsContainer}>
                 <div className={styles.titleSection}>
                     <h3>Technical Skills</h3>
-                    <h3>Soft Skills</h3>
+                    <p>My primary focus has been learning the core languages of the MERN stack, as well as peripheral technologies that help to increase the functionality of the apps that I build.</p>
+                    <p>For example, I have learned and implemented the basics of authentication with a server, using JSON Web Tokens stored in cookies. </p>
+                    {/* <h3>Soft Skills</h3> */}
                 </div>
                 <div className={styles.skillCardsContainer}>
                     <div className={styles.floatingSkillTitle}>
                         <h3>{activeSkillTitle}</h3>
+                        {/* <svg viewBox='0 0 200 100' width={200}>
+                            <ellipse cx={100} cy={50} rx={100} ry={30} fill="#cecece"/>
+                            <ellipse cx={100} cy={50} rx={80} ry={20} fill={"#141414"}/>
+                        </svg> */}
                     </div>
-                    <div style={{}} className={styles.skillCategoryCard} onClick={() => skillSectionClickHandler("frontend")}>
-                        <img src={frontEndSVG} className={styles.skillCategoryBgImg} loading="lazy"/>
-                        <ul className='skills-list__list'>
-                            <li>HTML5</li>
-                            <li>CSS3</li>
-                            <li>Javascript</li>
-                            <li>SCSS</li>
-                            <li>React.js</li>
-                            <li>Next.js</li>
-                        </ul>
-                    </div>
-                    <div style={{}} className={styles.skillCategoryCard} onClick={() => skillSectionClickHandler("backend")}>
-                        <img src={backEndSVG} className={styles.skillCategoryBgImg} loading="lazy"/>
-                        <ul>
-                            <li>Node.js</li>
-                            <li>Express.js</li>
-                            <li>Wordpress CMS</li>
-                            <li>MongoDB</li>
-                        </ul>
-                    </div>
-                    <div style={{}} className={styles.skillCategoryCard} onClick={() => skillSectionClickHandler("auxiliary")}>
-                        <img src={databaseSVG} className={styles.skillCategoryBgImg} loading="lazy"/>
-                        <ul>
-                            <li>Webpack</li>
-                            <li>Babel</li>
-                            <li>Typescript</li>
-                            <li>Jest</li>
-                        </ul>
-                    </div>
-                    <div style={{}} className={styles.skillCategoryCard} onClick={() => skillSectionClickHandler("design")}>
-                        <img src={easelSVG} className={styles.skillCategoryBgImg} loading="lazy"/>
-                        <ul>
-                            <li>AdobeXD</li>
-                            <li>GIMP</li>
-                            <li>XMind</li>
-                            <li>Inkscape</li>
-                        </ul>
-                    </div>
+                    <SkillCategoryCard 
+                        skillSectionClickHandler={skillSectionClickHandler}
+                        icon={{src: frontEndSVG, alt: 'alt text'}} 
+                        title={"frontend"} 
+                        skillsList={["HTML5", "CSS3", "Javascript", "SCSS", "React.js", "Next.js",]} 
+                        isActive={activeSkillTitle === "Frontend"}  
+                        cardIdx={0}
+                    />
+                    <SkillCategoryCard 
+                        skillSectionClickHandler={skillSectionClickHandler}
+                        icon={{src: backEndSVG, alt: 'alt text'}} 
+                        title={"backend"} 
+                        skillsList={["Node.js", "Express.js", "Wordpress CMS", "MongoDB"]} 
+                        isActive={activeSkillTitle === "Backend"}  
+                        cardIdx={1}
+                    />
+                    <SkillCategoryCard 
+                        skillSectionClickHandler={skillSectionClickHandler}
+                        icon={{src: databaseSVG, alt: 'alt text'}} 
+                        title={"auxiliary"} 
+                        skillsList={["Webpack", "Babel", "Typescript", "Jest",]} 
+                        isActive={activeSkillTitle === "Tools"} 
+                        cardIdx={2}
+                     />
+                    <SkillCategoryCard 
+                        skillSectionClickHandler={skillSectionClickHandler}
+                        icon={{src: easelSVG, alt: 'alt text'}} 
+                        title={"design"} 
+                        skillsList={["AdobeXD", "GIMP", "XMind", "Inkscape"]} 
+                        isActive={activeSkillTitle === "Design"} 
+                        cardIdx={3}
+                     />
                 </div> 
             </div>
         </section>
-    )
- }
+    );
+ };
 
  export default SkillsSection;
