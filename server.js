9843ac2cd6a25071c987fab6bb63dd713f77aaac
Tom Hayton <t.hayton37@gmail.com>
2022-02-09 20:49:08 +0000

    browserlists updated, heroku deployment script added, server.js added for heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('dist'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+    });
+};
+
+app.listen(PORT, () => {
+    console.log("App up and running on port: " + PORT);
+});
